026a7f0923111cd8642880fa1b0cdd6a98899a54
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-05 08:53:39 -0500

    Sharing SwiftUI state with @StateObject

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,27 +7,26 @@
 
 import SwiftUI
 
-class User {
-	var firstName = "Bilbo"
-	var lastName = "Baggins"
+class User: ObservableObject  {
+	@Published var firstName = "Bilbo"
+	@Published var lastName = "Baggins"
 }
 
 struct ContentView: View {
-	@State private var user = User()
-	
+	@StateObject var user = User()
 	var body: some View {
 		VStack{
 			Text("Your name is \(user.firstName) \(user.lastName)")
-				
+			
 			TextField("First name", text: $user.firstName)
 			TextField("Lastname", text: $user.lastName)
 		}
-     
-    }
+		
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
