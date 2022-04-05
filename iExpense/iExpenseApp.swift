dd0179174ce25a3f66e2d27900b8a6376b893722
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-05 08:04:56 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ross Phillips on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
