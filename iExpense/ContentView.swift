43386f1a07d8d8d7ebe414793876c270087eccb3
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-06 10:43:02 -0500

    Working with Identifiable items in SwiftUI

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,22 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var numbers = [Int]()
-	@State private var currentNumber = 1
+	@StateObject var expenses = Expenses()
 
 	var body: some View {
-		VStack {
+		NavigationView {
 			List {
-				ForEach(numbers, id: \.self) {
-					Text("Row \($0)")
+				ForEach(expenses.items) { item in
+					Text(item.name)
+				}
+				.onDelete(perform: removeItems)
+			}
+			.navigationTitle("iExpense")
+			.toolbar {
+				Button {
+					let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+					expenses.items.append(expense)
+				} label: {
+					Image(systemName: "plus")
 				}
 			}
-
-			Button("Add Number") {
-				numbers.append(currentNumber)
-				currentNumber += 1
-			}
 		}
+		
+	}
+	func removeItems(at offsets: IndexSet) {
+		expenses.items.remove(atOffsets: offsets)
 	}
 }
 
